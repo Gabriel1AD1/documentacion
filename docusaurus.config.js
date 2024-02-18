8845bb2edec693737d9d03720c89478d04f9b6c9
Gabriel AD <cerroteberes@gmail.com>
2024-02-18 03:12:34 -0500

    Cambio de imagen

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://gabriel1ad1.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/documentacion/',
@@ -63,7 +63,7 @@ const config = {
         title: '',
         logo: {
           alt: 'Documentación de Chat Buho',
-          src: 'https://buho.la/images/chat/chat-buho.svg',
+          src: 'https://pngimg.com/uploads/github/github_PNG28.png',
         },
         items: [
           // {
